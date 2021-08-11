e94f4c8ad230681fa074b767b4103c57c6790102
Luiz Felipe Sampaio da Fonseca <luizffons@hotmail.com>
2021-08-11 09:59:22 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,36 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+
+        public DataService(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public void InicializaDB()
+        {
+            contexto.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+            SaveProdutos(livros);
+        }
+
+        
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+   
+}
